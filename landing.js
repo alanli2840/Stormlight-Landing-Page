c4f3e2f4304a333aa9aaf635b22093e6f9049b32
alan li <alanli1ny@gmail.com>
2023-02-13 12:29:15 -0500

    Updated method of hiding bgs and added option to toggle visibility of intro-box on click

--- landing.js
+++ landing.js
@@ -1,7 +1,8 @@
 const bgProgBar = document.querySelector(".bg-progress-bar");
 const bgVids = document.querySelectorAll(".background-video");
+const introBox = document.querySelector(".intro-box");
+const introSection = document.querySelector("intro");
 
-const opacitys = [1, 0, 0, 0];
 let bgIndex = 1;
 let progCount = 0.0;
 
@@ -9,17 +10,17 @@ const updateBg = () => {
     let prevIndex = bgIndex - 1;
     let curIndex = bgIndex;
     if(bgIndex === 0) {
-        prevIndex = opacitys.length - 1;
+        prevIndex = bgVids.length - 1;
         curIndex = 0;
     }
-    if(bgIndex === opacitys.length - 1) {
+    if(bgIndex === bgVids.length - 1) {
         bgIndex = 0;
     }
     else {
         bgIndex++;
     }
-    bgVids[curIndex].style.opacity = ++opacitys[curIndex]; 
-    bgVids[prevIndex].style.opacity = --opacitys[prevIndex];
+    bgVids[curIndex].classList.toggle('hidden');
+    bgVids[prevIndex].classList.toggle('hidden');
 }
 
 const moveProg = () => {
@@ -30,6 +31,13 @@ const moveProg = () => {
     progCount += .1;
     bgProgBar.style.transform =`scaleX(${progCount}%)`;
     setTimeout(moveProg, 1);
-}
+};
 
 document.addEventListener('DOMContentLoaded', moveProg);
+
+const toggleIntroBox = () => {
+    introBox.classList.toggle('opacity-zero');
+};
+
+introBox.addEventListener('click', toggleIntroBox);
+introSection.addEventListener('click', toggleIntroBox);
